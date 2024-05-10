e0d398cfa0dbeddea62fc6436f111c4809bcb029
freed2003 <gary.song2003@gmail.com>
2024-05-10 11:53:26 -0700

    more cases added

--- test.py
+++ test.py
@@ -2,16 +2,43 @@ from mutators.llm import LLMMutator
 from mutators.mask_fill import MaskFillMutator
 from mutators.span_fill import SpanFillMutator
 from oracle import SoloOracle, JointOracle, RelativeOracle, RankOracle
+from model_builders.pipeline import PipeLineBuilder
+import hydra
+import os
 import csv
-
-mutator = SpanFillMutator()
-
-oracle = SoloOracle()
-results = []
-with open("./tests/mutator/tests_v1.csv") as csvfile:
-    reader = csv.reader(csvfile, delimiter=",")
-    for row in reader:
-        print(row)
-        _, prompt, response = row
-        mutated = mutator.mutate(response)
-        results.append(oracle.is_quality_preserved(prompt, response, mutated))
+templates = [
+        # ("rate.self-reward", SoloOracle), 
+        ("solo.lmsys.ia", SoloOracle), 
+        ("solo.lmsys.ib", SoloOracle), 
+        ("rank.alpaca_eval", RankOracle), 
+        ("joint.lmsys.ia", JointOracle), 
+        ("joint.lmsys.ib", JointOracle), 
+        ("relative.sandpaper.3", RelativeOracle), 
+        ("relative.sandpaper.5", RelativeOracle), 
+    ]
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg):
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(cfg.attack_args.cuda)
+    os.environ["WORLD_SIZE"] = str(len(str(cfg.attack_args.cuda).split(",")))
+    cfg.mutator_args.type = "llm"
+    mutators = [SpanFillMutator(), MaskFillMutator(), LLMMutator(cfg.mutator_args)]
+    mutalabel = ["SpanFill, MaskFIll", "LLM"]
+    oracles = []
+    oraclabel = []
+    for template, Oracle in templates:
+        cfg.oracle_args.template = template
+        oracle = Oracle(cfg.oracle_args)
+        oraclabel.append(template)
+    tests = []
+    results = {}
+    with open("./tests/mutator/tests_v1.csv") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            _, prompt, response = row
+            tests.append([_, prompt, response])
+    for i, mutator in enumerate(mutators):
+        for j, oracle in enumerate(oracles):
+            for _, prompt, response in tests:
+                results[f"{oraclabel[j]+{mutalabel[i]}}"] = []
+                mutated = mutator.mutate(response)
+                results[f"{oraclabel[j]+{mutalabel[i]}}"].append(oracle.is_quality_preserved(prompt, response, mutated))
