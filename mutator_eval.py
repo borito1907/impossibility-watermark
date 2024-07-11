c61415bc09906eb6e846786277cf29eedd783880
Gary Song <41763498+freed2003@users.noreply.github.com>
2024-07-10 21:47:01 -0600

    Update mutator_eval.py

--- mutator_eval.py
+++ mutator_eval.py
@@ -5,6 +5,7 @@ import hydra
 from tqdm import tqdm
 import logging
 from oracles.absolute import PrometheusAbsoluteOracle
+import matplotlib.pyplot as plt
 
 log = logging.getLogger(__name__)
 
@@ -127,6 +128,35 @@ def eval(cfg):
                 log.info("Saving results to csv...")
                 df = pd.DataFrame(results)
                 df.to_csv("./results/mutator_eval.csv", index=False)
-
+    tests_df = pd.read_csv("./results/mutator_eval.csv")
+    data = {}
+    output = []
+    for index, row in tqdm(tests_df.iterrows(), desc='Tests'):
+        if row["mutator"] not in data:
+            data[row["mutator"]] = {row["mutation_step"]: [0,0]}
+        if row["mutation_step"] not in data[row["mutator"]]:
+            data[row["mutator"]][row["mutation_step"]] = [0,0]
+        data[row["mutator"]][row["mutation_step"]][1] += 1
+        if row["quality_preserved"]:
+            data[row["mutator"]][row["mutation_step"]][0] += 1
+    print(data)
+    for i in data:
+        for j in data[i]:
+            temp = {}
+            temp["mutator"] = i
+            temp["mutation step"] = j
+            temp["percent preserved"] = data[i][j][0]/ data[i][j][1]
+            output.append(temp)
+    df = pd.DataFrame(output)
+    df.to_csv("./results/mutator_percent.csv")
+    
+    fig, ax = plt.subplots(len(data))
+    for i, mut in enumerate(data.keys()):
+        ax[i].plot(data[mut].keys(), [i[0]/i[1] for i in data[mut].values()])
+        ax[i].set_title(f"Percentage vs steps for {mut}")
+    fig.tight_layout(pad=5.0)
+    plt.show()
+    plt.savefig("mutator.png")
+    
 if __name__ == "__main__":
     eval()
