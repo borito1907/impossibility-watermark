0df78521a50ab65707beeba83e95a64bfa21e6ac
Fabrice Harel-Canada <fabricehc@pluslab-a6000.cs.ucla.edu>
2024-07-15 14:08:56 -0700

    adding modifications for new prometheus updates for absolute oracle (relative pending), resulting in 12s test runtime

--- oracles/__init__.py
+++ oracles/__init__.py
@@ -5,11 +5,7 @@
 #     SoloOracle
 # )
 
+from .base import Oracle
 from .absolute import PrometheusAbsoluteOracle
 from .relative import PrometheusRelativeOracle
-
-from .custom import (
-    Oracle
-)
-
 from .relative3 import RelativeOracle3
---
+++ oracles/base.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from guidance import models
+
+# Abstract base class for all oracles
+class Oracle(ABC):
+    
+    judge = None
+
+    def __init__(self, cfg) -> None:
+        self.cfg = cfg # config.oracle_args
+
+    def _initialize_llm(self):
+        log.info("Initializing a new Oracle model from cfg...")
+        if "gpt-" in self.cfg.model_id:
+            llm = models.OpenAI(
+                self.cfg.model_id,
+                echo=False
+            )
+        else:
+            llm = models.Transformers(
+                self.cfg.model_id, 
+                echo=False,
+                cache_dir=self.cfg.model_cache_dir, 
+                device_map=self.cfg.device_map
+            )
+        return llm
+    
+    @abstractmethod
+    def evaluate(self, instruction, output_1, output_2, **kwargs):
+        pass
+
+    @abstractmethod
+    def extract_label(self, evaluation):
+        pass
+
+    @abstractmethod
+    def test(self, instruction, output_1, output_2, label, **kwargs):
+        pass
+    
