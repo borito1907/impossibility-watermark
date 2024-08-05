71d8e80b6648c12ae8a1c2ffe877b9a5eaaed062
Boran Erol boranerol@ucla.edu <borito1907@pluslab-a6000.cs.ucla.edu>
2024-08-05 12:49:21 -0700

    fixed UMD generations

--- strip_umd.py
+++ strip_umd.py
@@ -7,38 +7,33 @@ from utils import save_to_csv, get_prompt_or_output, get_prompt_and_id_dev, get_
 log = logging.getLogger(__name__)
 
 def remove_after_assistant(input_string):
-    if input_string.startswith("system"):
-        # Find the second occurrence of "assistant"
-        first_occurrence = input_string.find("assistant")
-        if first_occurrence != -1:
-            second_occurrence = input_string.find("assistant", first_occurrence + 1)
-            if second_occurrence != -1:
-                return input_string[:second_occurrence]
-            else:
-                return input_string[:first_occurrence]
-    else:
-        # Find the first occurrence of "assistant"
-        first_occurrence = input_string.find("assistant")
-        if first_occurrence != -1:
-            return input_string[:first_occurrence]
-    
+    prefix = "system\n\nYou are a helpful personal assistant.user\n\nassistant\n\n"
+    input_string = input_string.removeprefix(prefix)
+
+    first_occurrence = input_string.find("assistant\n\n")
+    if first_occurrence != -1:
+        return input_string[:first_occurrence]
     return input_string
 
 @hydra.main(version_base=None, config_path="conf", config_name="gen_conf")
 def main(cfg):
-    new_file_path = '/local1/borito1907/impossibility-watermark/inputs/test_umd/stripped_watermarked_texts.csv'
+    folder = 'dev_umd_long'
+    new_file_path = f'/local1/borito1907/impossibility-watermark/inputs/{folder}/stripped_watermarked_texts.csv'
 
     cfg.watermark_args.only_detect = True
 
-    folders = [f"umd_test_{partition}" for partition in range(1,8)]
+    # folders = [f"umd_test_{partition}" for partition in range(1,8)]
     
-    test_dfs = []
+    # test_dfs = []
 
-    for folder in folders:
-        path = f"/local1/borito1907/impossibility-watermark/inputs/{folder}/watermarked_texts.csv"
-        df = pd.read_csv(path)
-        test_dfs.append(df)
-    df = pd.concat(test_dfs, axis=0)
+    # for folder in folders:
+    #     path = f"/local1/borito1907/impossibility-watermark/inputs/{folder}/watermarked_texts.csv"
+    #     df = pd.read_csv(path)
+    #     test_dfs.append(df)
+    # df = pd.concat(test_dfs, axis=0)
+
+    path = f'/local1/borito1907/impossibility-watermark/inputs/{folder}/watermarked_texts.csv'
+    df = pd.read_csv(path)
 
     df['text_stripped'] = df['text'].apply(remove_after_assistant)
 
