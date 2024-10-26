85dba3acac6632a41d2b32c362760cfe62e86404
Fabrice Harel-Canada <fabricehc@pluslab-a6000.cs.ucla.edu>
2024-10-26 16:46:47 -0700

    adding attack traces for span, sentence, doc1, doc2 for umd_new

--- attack_traces/scripts/split.py
+++ attack_traces/scripts/split.py
@@ -8,7 +8,7 @@ def split_csv(input_file, output_file_base):
     df = pd.read_csv(input_file)
     
     # Split the data into 10 chunks
-    chunks = np.array_split(df, 10)
+    chunks = np.array_split(df, 2)
     
     # Write each chunk to a new CSV file with numbers 1-10 appended
     for i, chunk in enumerate(chunks, 1):
@@ -17,7 +17,7 @@ def split_csv(input_file, output_file_base):
         print(f"Saved {output_file}")
 
 # Usage
-input_file = './attack_traces/DiffOracle_umd_new_WordMutator_n-steps=1000_attack_results.csv'
-output_file_base = './attack_traces/DiffOracle_umd_new_WordMutator_n-steps=1000_attack_results'
+input_file = './attack_traces/DiffOracle_umd_new_SpanMutator_n-steps=200_attack_results.csv'
+output_file_base = './attack_traces/DiffOracle_umd_new_SpanMutator_n-steps=200_attack_results.csv'
 
 split_csv(input_file, output_file_base)
